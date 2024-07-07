61cbd8da6bbb03b12bd99e4af787f160ff676217
Romano Zabini <romano.zabini@gmail.com>
2024-07-07 10:57:49 +0200

    add test for wrong password decryption

--- src/test/groovy/test/com/github/rzabini/EncryptedPropertiesSpecification.groovy
+++ src/test/groovy/test/com/github/rzabini/EncryptedPropertiesSpecification.groovy
@@ -1,5 +1,6 @@
 package test.com.github.rzabini
 
+import com.github.rzabini.AESUtil
 import com.github.rzabini.EncryptedProperties
 import spock.lang.Specification
 
@@ -8,20 +9,60 @@ import java.nio.file.Path
 
 class EncryptedPropertiesSpecification extends Specification {
 
-    def go() {
+    def "should encrypt password entries in property file"() {
+        setup:
         Path inputFile = Files.createTempFile("sample", ".properties")
         inputFile.toFile().text =
         '''
         username=test
         password=encryptme
         '''.replaceAll('\t','')
+
+        when:
         Properties encryptedProperties = EncryptedProperties.create("master", inputFile)
-        expect:
+
+        then:
         encryptedProperties.getProperty('username') == 'test'
         encryptedProperties.getProperty('password') == 'encryptme'
-        println inputFile.toFile().filterLine {line -> line.startsWith('pazzword=ENC(')}
+        inputFile.toFile().readLines().find({line -> line.startsWith('password=ENC(')})
 
         cleanup:
         Files.deleteIfExists(inputFile)
+    }
+
+    def "should decrypt password entries in property file"(){
+        setup:
+        Path inputFile = Files.createTempFile("sample", ".properties")
+        initializeEncryptedPropertiesFile(inputFile)
+
+        when:
+        Properties encryptedProperties = EncryptedProperties.create("mymasterpassword", inputFile)
+
+        then:
+        encryptedProperties.getProperty('username') == 'myusername'
+        encryptedProperties.getProperty('password') == 'secret'
+
+        cleanup:
+        Files.deleteIfExists(inputFile)
+    }
+
+    def "should throw an exception if trying to decrypt with a wrong password"(){
+        setup:
+        Path inputFile = Files.createTempFile("sample", ".properties")
+        initializeEncryptedPropertiesFile(inputFile)
+
+        when:
+        Properties encryptedProperties = EncryptedProperties.create("wrong", inputFile)
+
+        then:
+        thrown(IllegalStateException)
+    }
+
+    private initializeEncryptedPropertiesFile(Path inputFile) {
+        inputFile.toFile().text =
+                '''
+        username=myusername
+        password=ENC(mhKSaCaq3rtBOowk7+X+9ghO9U6Qufusn8jV0v9nJac=)
+        '''.replaceAll('\t', '')
     }
 }
